95fb748bf7c4e205c1ae6beda8a8ade59a9d1b74
David <zhongdavid65@gmail.com>
2025-04-22 16:16:29 -0300

    oiii

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start');
+//Configuring the menu
+for (let i= 4; i <= 10; i+=2) {//From 4 to 10
+    let n = i*i;//Get i² 2
+    //Create a new option for the select
+    let op = document.createElement('option');
+    //Set both value and content to i2
+    op.value = n;
+    op.innerHTML = n;
+    //Add the new option to the page
+    select.appendChild(op);
+    }
+    
